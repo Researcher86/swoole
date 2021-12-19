48ecaf7de658250260a802765530a4b34f181c42
Tanat <researcher2286@gmail.com>
2021-12-19 20:40:21 +0600

    Add task worker

--- src/public/index.php
+++ src/public/index.php
@@ -11,6 +11,7 @@ Co::set(['hook_flags'=> SWOOLE_HOOK_ALL]);
 $http = new Server('0.0.0.0', 9501);
 $http->set([
     'worker_num' => 4,
+    'task_worker_num' => 4,
     'document_root' => '/app/src/public/web/',
     'enable_static_handler' => true,
 //    'static_handler_locations' => ['/static', '/app/images'],
@@ -20,28 +21,33 @@ $http->on('start', function (Server $server) {
     echo "Swoole http server is started at http://0.0.0.0:9501\n";
 });
 
-$http->on('request', function (Request $request, Response $response) {
+$http->on('request', function (Request $request, Response $response) use ($http) {
     $response->header('Content-Type', 'text/html; charset=utf-8');
+    $http->task('Hello World', 0);
 //    $response->sendfile('/app/src/public/web/index.html');
 
-//    Co\run(function()
-//    {
-        go(function()
-        {
-            sleep(5);
-//            Co::sleep(5);
-            echo "Done 1\n";
-        });
-
-        go(function()
-        {
-            sleep(10);
-//            Co::sleep(10);
-            echo "Done 2\n";
-        });
-//    });
+////    Co\run(function()
+////    {
+//        go(function()
+//        {
+//            sleep(5);
+////            Co::sleep(5);
+//            echo "Done 1\n";
+//        });
+//
+//        go(function()
+//        {
+//            sleep(10);
+////            Co::sleep(10);
+//            echo "Done 2\n";
+//        });
+////    });
 
     $response->end('<h1>Hello World!</h1>');
+});
+
+$http->on('task', function (Swoole\Server $server, $task_id, $reactorId, $data) {
+    var_dump($data);
 });
 
 $process = new Swoole\Process(function () {
